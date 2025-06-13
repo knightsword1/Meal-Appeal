dbe60c83877b62cd92451611c14d4975ea454972
Ayush <sinhaayush1125@gmail.com>
2025-06-13 15:23:45 +0530

    updated fetch code in server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 const app = express();
 const PORT = process.env.PORT || 5000;
